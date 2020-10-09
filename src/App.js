92e217c5d3161ced07258003df22d51036b0b321
nellidimitrowa <nellidimitrowa@gmail.com>
2020-10-09 16:12:24 +0300

    Add Search component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import About from './components/About/About';
+import Search from './components/Search/Search';
 
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
+        <Search />
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
       </div>
