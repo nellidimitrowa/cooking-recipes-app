fa775d374c236f7237a1490373682994e3a68627
nedyalka.dimitrova <nedyalka.dimitrova@fadata.eu>
2020-10-18 22:44:45 +0300

    Create add comment functionality and delete recipe

---
+++ src/components/Comments/Comments.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import './Comments.css';
+
+
+function Comments() {
+  const [newComment, setNewComment] = useState("");
+  const [comments, setComments] = useState([]);
+  const { id } = useParams();
+
+  const getData = async () => {
+    const result = await axios.get(`http://localhost:3000/comments?recipeId=${id}`);
+    return result.data;
+  }
+
+  useEffect(() => {
+    getData().then(comments => setComments(comments));
+  }, []);
+
+  const onSubmit= () => {
+    const data = {
+      recipeId: id,
+      username: "ann",
+      date: "18.10.2020 18:15",
+      text: newComment
+    };
+    axios.post(`http://localhost:3000/comments`, data)
+      .then(result => {
+        console.log(result);
+      })
+    setNewComment("");
+  }
+
+  function handleTextChanged(event) {
+    setNewComment(event.target.value);
+  }
+
+  return (
+    <div>
+      <hr /><hr />
+      <h1 className="comments">Comments</h1>
+      <ul>
+        {
+          comments ?
+            comments.map((comment, index) => (<li className="comments-form" key={index}>
+              <label>{comment.username}</label> <br></br> {comment.text}</li>)) :
+            "Loading..."
+        }
+      </ul>
+      <form className="add-comment-form" onSubmit={onSubmit}>
+        <input type="text" placeholder="Type a comment" autoComplete="off"
+          name="comment-text" value={newComment} onChange={handleTextChanged} />
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  )
+}
+
+export default Comments;
--- src/components/RecipeDetails/RecipeDetails.js
+++ src/components/RecipeDetails/RecipeDetails.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import axios from 'axios';
 import './RecipeDetails.css';
+import Comments from '../Comments/Comments';
 
-function RecipeDetails() {
+const RecipeDetails = () => {
     const [recipe, setRecipe] = useState([]);
     const { id } = useParams();
+    const history = useHistory()
     const recipeUrl = `http://localhost:3000/recipes/${id}`;
-    
+
     const getData = async () => {
         const result = await axios.get(recipeUrl);
         return result.data;
@@ -15,24 +17,30 @@ function RecipeDetails() {
 
     useEffect(() => {
         getData().then(recipe => setRecipe(recipe));
-      }, []);
+    }, []);
+
+    function handleDelete() {
+        axios.delete(recipeUrl);
+        history.goBack();
+    }
 
     return (
         <div className="container">
             <div className="active-recipe">
-                <img className="active-recipe_img" src={recipe.imagePath}></img>
-                <button className="active-recipe_button">Delete</button>
-                <button className="active-recipe_button">Edit</button>
                 <h1 className="active-recipe_title">{recipe.name}</h1>
                 <h5 className="active-recipe_description">{recipe.description}</h5>
-                <h2 className="active-recipe_ingredients">Ingredients:</h2>
+                <img className="active-recipe_img" src={recipe.imagePath}></img>
+                <button className="active-recipe_button" onClick={handleDelete}>Delete</button>
+                <button className="active-recipe_button">Edit</button>
+                <h2 className="active-recipe_ingredients">Ingredients</h2>
                 <ul>
-                    {   
+                    {
                         recipe.ingredients ?
-                        recipe.ingredients.map((ingredient, index) => (<li className="active-recipe_ingredient" key={index}>{ingredient.amount} {ingredient.name}</li>)) :
-                        "Loading..."
+                            recipe.ingredients.map((ingredient, index) => (<li className="active-recipe_ingredient" key={index}>{ingredient.amount} {ingredient.name}</li>)) :
+                            "Loading..."
                     }
                 </ul>
+                <Comments recipe={recipe} />
             </div>
         </div>
     )
