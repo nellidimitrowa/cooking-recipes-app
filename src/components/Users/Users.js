bc52e296b982a08c33a24fc1d906c5087c4a0474
nellidimitrowa <nellidimitrowa@gmail.com>
2020-10-20 01:11:27 +0300

    Create Users page

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Users.css";
 
 function Users() {
-    return(
-        <div>
-            <h3>Hi</h3>
-        </div>
-    )
+  const [users, setUsers] = useState([]);
+
+  const getData = async () => {
+    const result = await axios.get(`http://localhost:3000/users`);
+    return result.data;
+  };
+
+  useEffect(() => {
+    getData().then((users) => setUsers(users));
+  }, []);
+
+  return (
+    <div>
+      <table className="users">
+        <tr>
+          <th>Username</th>
+          <th>Email</th>
+          <th>Role</th>
+        </tr>
+        {users
+          ? users.map((user, index) => (
+              <tr key={index}>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>{user.role}</td>
+              </tr>
+            ))
+          : "Loading..."}
+      </table>
+    </div>
+  );
 }
 
 export default Users;
